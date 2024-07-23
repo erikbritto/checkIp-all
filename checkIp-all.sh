4c773d2db50135ddba270bb9e0bd5c140f3f5875
erikbritto <erikbritto@gmail.com>
2024-07-23 09:22:29 +0200

    Fixed typo

--- checkIp-all.sh
+++ checkIp-all.sh
@@ -5,7 +5,7 @@ if [ "$#" != "3" ] || [ "$UID" != "0" ]
 It needs an IP source address followed by a destination address and
 target  port.
 All source ports will be tested against the destination address
-using the target Peort.
+using the target port.
 
 	Usage: sudo checkIP-all.sh sourceIP destinationIp targetPort
 	"
@@ -14,7 +14,7 @@ fi
 for N in {1..65535..1}
 do
   # TCP  add --hide-sent
-  nping --hide-sent --delay 1s --tcp-connect -c 1 --dest-port $N --source-ip $1 --dest-ip $2 -source-port $3
+  nping --delay 0s --tcp-connect -c 1 --dest-port $N --source-ip $1 --dest-ip $2 -source-port $3
   # UDP 
   #nping --delay 1s --udp -c 1 --dest-port $N --source-ip $1 --dest-ip $2 --source-port $3
 done
